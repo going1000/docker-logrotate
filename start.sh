168579008252ead76a02852343b512f1075127c6
going1000 <miaorenjin@gmail.com>
2021-05-10 20:04:27 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,6 +13,20 @@ ${LOGROTATE_LOGFILES}
   copytruncate
   rotate ${LOGROTATE_FILENUM}
 }
+
+/var/log/cron
+/var/log/maillog
+/var/log/messages
+/var/log/secure
+/var/log/spooler
+{
+    size 1G
+    missingok
+    sharedscripts
+    postrotate
+	      /bin/kill -HUP `cat /var/run/syslogd.pid 2> /dev/null` 2> /dev/null || true
+    endscript
+}
 EOF
 
 if [ -z "$CRON_EXPR" ]; then
